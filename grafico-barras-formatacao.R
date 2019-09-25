5c0515e12d6c90a3c579abc5a64d4ad97c47e2e6
leo-filgueira <leonardo_filgueira@id.uff.br>
2019-09-24 22:55:59 -0300

    Muda título e nome de eixo y, adiciona exemplo com 2 cores rgb e exemplo com xlim

--- grafico-barras-formatacao.R
+++ grafico-barras-formatacao.R
@@ -3,48 +3,71 @@
 
 ## Gráfico de barras - Formatação do gráfico
 
-x<-c("feminino", "masculino")
-RJ<-c(3500,1990)
+x <- c("Feminino", "Masculino")
+RJ <- c(3500, 1990)
 
 # Alterando cores das barras:
 
-## col="nome da cor" ou col=valor - Altera a cor das barras
+## col: Altera a cor das barras
 
-barplot(RJ, names.arg=x, main= "Entrevistados/ sexo e estado", xlab= "Sexo", ylab="Quantidade", sub="Dados fictícios", col="green")
+barplot(RJ, names.arg = x, main = "Sexo dos entrevistados do RJ", xlab = "Sexo", 
+        ylab = "Quantidade de entrevistados", sub = "Dados fictícios", col = "green")
 
-barplot(RJ, names.arg=x, main= "Entrevistados/ sexo e estado", xlab= "Sexo", ylab="Quantidade", sub="Dados fictícios", col= 2)
+barplot(RJ, names.arg = x, main = "Sexo dos entrevistados do RJ", xlab = "Sexo", 
+        ylab = "Quantidade de entrevistados", sub = "Dados fictícios", col = 2)
 
 ## Caso queiramos uma barra de cada cor:
 
-barplot(RJ, names.arg=x, main= "Entrevistados/ sexo e estado", xlab= "Sexo", ylab="Quantidade", sub="Dados fictícios", col=c(10,20))
+barplot(RJ, names.arg = x, main = "Sexo dos entrevistados do RJ", xlab = "Sexo", 
+        ylab = "Quantidade de entrevistados", sub = "Dados fictícios", col = c(10, 20))
+
+## Utilizando o sistema de cores RGB: 
+## Devemos combinar valores entre 0 e 1 numa sequência tripla
+
+barplot(RJ, names.arg = x, main = "Sexo dos entrevistados do RJ", xlab = "Sexo", 
+        ylab = "Quantidade de entrevistados", sub = "Dados fictícios",
+        col = rgb(0.8, 0.7, 0.2))
+
+# Também é possível passar uma cor por barra:
+barplot(RJ, names.arg = x, main = "Sexo dos entrevistados do RJ", xlab = "Sexo", 
+        ylab = "Quantidade de entrevistados", sub = "Dados fictícios",
+        col = c(rgb(0.9, 0, 0.3), rgb(0, 0.9, 0.3)))
 
 
-## Utilizando o sistema de cores RGB **Devemos combinar valores entre 0 e 1 numa sequência tripla
+## border = NA - Elimina a linha de borda das barras
 
-barplot(RJ, names.arg=x, main= "Entrevistados/ sexo e estado", xlab= "Sexo", ylab="Quantidade", sub="Dados fictícios",col=rgb(0,0.9,0.3))
-barplot(RJ, names.arg=x, main= "Entrevistados/ sexo e estado", xlab= "Sexo", ylab="Quantidade", sub="Dados fictícios",col=rgb(0.8,0.7,0.2))
-
-## border=NA - Elimina a linha de borda das barras
-
-barplot(RJ, names.arg=x, main= "Entrevistados/ sexo e estado", xlab= "Sexo", ylab="Quantidade", sub="Dados fictícios", border=NA)
+barplot(RJ, names.arg = x, main = "Sexo dos entrevistados do RJ", xlab = "Sexo", 
+        ylab = "Quantidade de entrevistados", sub = "Dados fictícios", border = NA)
 
 ## Gráfico de barras com duas variáveis
 
-x<-c("feminino", "masculino")
-RJ<-c(3500,1990)
-SP<-c(2000,3455)
-Y<-matrix(c(RJ,SP), nrow=2, ncol=2, dimnames=list(x, c("Rio de Janeiro","São Paulo")))
-barplot(Y, main= "Entrevistados/sexo e estado", ylab="Quantidade")
+x <- c("Feminino", "Masculino")
+RJ <- c(3500, 1990)
+SP <- c(2000, 3455)
+Y <- matrix(c(RJ, SP), nrow = 2, ncol = 2, 
+            dimnames = list(x, c("Rio de Janeiro", "São Paulo")))
+Y
 
-## beside=T - Para se obter barras lado a lado
+barplot(Y, main = "Sexo e estado dos entrevistados", ylab = "Quantidade de entrevistados")
 
-barplot(Y, main= "Entrevistados/sexo e estado", ylab="Quantidade", beside=T)
+## beside = T - Para se obter barras lado a lado
+
+barplot(Y, main = "Sexo e estado dos entrevistados", ylab = "Quantidade de entrevistados", 
+        beside = T)
 
 ## Adicionando Legenda
 
-barplot(Y, main= "Entrevistados/sexo e estado", ylab="Quantidade", beside=T, legend.text=rownames(Y),sub="Fonte: Dados fictícios")
+barplot(Y, main = "Sexo e estado dos entrevistados", ylab = "Quantidade de entrevistados", 
+        beside = T, legend.text = rownames(Y), sub = "Dados fictícios")
 
-## ylim=c(valor inicial, valor final) - para aumentar o limite do eixo y
+## ylim: para mudar o limite do eixo y
 
-barplot(Y, main= "Entrevistados/sexo e estado",xlab= "Sexo", ylab="Quantidade", beside=T, legend.text=rownames(Y),
-        sub="Fonte: Dados fictícios", ylim=c(0, 6000))
+barplot(Y, main = "Sexo e estado dos entrevistados", xlab = "Sexo", 
+        ylab = "Quantidade de entrevistados", beside = T, legend.text = rownames(Y),
+        sub = "Dados fictícios", ylim = c(0, 6000))
+
+## xlim: para mudar o limite do eixo x
+
+barplot(Y, main = "Sexo e estado dos entrevistados", xlab = "Sexo", 
+        ylab = "Quantidade de entrevistados", beside = T, legend.text = rownames(Y),
+        sub = "Dados fictícios", xlim = c(0, 10))
