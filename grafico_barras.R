5cfe9bc27357fa4167cf6a078d8eff340382a7e3
leo-filgueira <leonardo_filgueira@id.uff.br>
2019-09-24 22:38:08 -0300

    Boas práticas de escrita e início com exemplo mais simples

--- grafico_barras.R
+++ grafico_barras.R
@@ -3,46 +3,61 @@
 
 ## Gráfico de barras
 
-# Sem subtítulo
+# Sem nenhuma formatação
 
-alunos <-c(1000,650,250)
-escolaridade<- c("Fundamental", "Médio", "Superior")
-barplot(alunos, names.arg=escolaridade, main= "Grau de Escolaridade", ylab="Quantidade de alunos")
+alunos <- c(1000, 650, 250)
+barplot(alunos)
 
-# Com subtítulo
+# Adicionando informações ao gráfico
 
-barplot(alunos, names.arg=escolaridade, main= "Grau de Escolaridade", ylab="Quantidade de alunos", sub="Dados fictícios")
+escolaridade <- c("Fundamental", "Médio", "Superior")
+barplot(alunos, names.arg = escolaridade, main= "Grau de Escolaridade",
+        ylab = "Quantidade de alunos")
 
-## cex.nome - Muda tamanho da fonte
+# Adicionando subtítulo
+
+barplot(alunos, names.arg = escolaridade, main = "Grau de Escolaridade", 
+        ylab = "Quantidade de alunos", sub = "Dados fictícios")
+
+## Mudando tamanho da fonte
 
 # Para o título
 
-barplot(alunos, names.arg=escolaridade, main= "Grau de Escolaridade", ylab="Quantidade de alunos", sub="Dados fictícios", cex.main= 2)
+barplot(alunos, names.arg = escolaridade, main = "Grau de Escolaridade", 
+        ylab = "Quantidade de alunos", sub = "Dados fictícios", cex.main = 2)
 
 # Para os eixos
 
-barplot(alunos, names.arg=escolaridade, main= "Grau de Escolaridade", ylab="Quantidade de alunos", sub="Dados fictícios", cex.lab = 1,5)
+barplot(alunos, names.arg = escolaridade, main = "Grau de Escolaridade", 
+        ylab = "Quantidade de alunos", sub = "Dados fictícios", cex.lab = 1.5)
 
-# Para o número dos eixos
+# Para o número do eixo horizontal
 
-barplot(alunos, names.arg=escolaridade, main= "Grau de Escolaridade", ylab="Quantidade de alunos", sub="Dados fictícios", cex.axis=2)
+barplot(alunos, names.arg = escolaridade, main= "Grau de Escolaridade", 
+        ylab = "Quantidade de alunos", sub = "Dados fictícios", cex.axis = 2)
 
 # Para o subtítulo
 
-barplot(alunos, names.arg=escolaridade, main= "Grau de Escolaridade", ylab="Quantidade de alunos", sub="Dados fictícios", cex.sub =2)
+barplot(alunos, names.arg = escolaridade, main = "Grau de Escolaridade", 
+        ylab = "Quantidade de alunos", sub = "Dados fictícios", cex.sub = 2)
 
-## axes=F - Elimina os eixos do gráfico 
+## axes = F: Elimina os eixos do gráfico 
 
-barplot(alunos, names.arg=escolaridade, main= "Grau de Escolaridade", ylab="Quantidade de alunos", axes=F)
+barplot(alunos, names.arg = escolaridade, main = "Grau de Escolaridade", 
+        ylab = "Quantidade de alunos", axes = F)
 
-## horiz=T - Inverte a posição das barras
+## horiz = T: Inverte a posição das barras
 
-barplot(alunos, names.arg=escolaridade, main= "Grau de Escolaridade", xlab= "Quantidade de alunos", sub="Dados fictícios", horiz=T)
+barplot(alunos, names.arg = escolaridade, main = "Grau de Escolaridade", 
+        xlab = "Quantidade de alunos", sub = "Dados fictícios", horiz = T)
 
-## density=valor - Altera o sombreamento das barras
+## density: Altera o sombreamento das barras
 
-barplot(alunos, names.arg=escolaridade, main= "Grau de Escolaridade", ylab="Quantidade de alunos", sub="Dados fictícios", density=20)
+barplot(alunos, names.arg = escolaridade, main = "Grau de Escolaridade", 
+        ylab = "Quantidade de alunos", sub = "Dados fictícios", density = 20)
 
-## angle=valor - Muda ângulo de inclinação das linhas (valor entre 0 e 360)
+## angle = valor - Muda ângulo de inclinação das linhas que preenchem as barras
+# (valor entre 0 e 360)
 
-barplot(alunos, names.arg=escolaridade, main= "Grau de Escolaridade", ylab="Quantidade de alunos", sub="Dados fictícios",density=20, angle=180)
+barplot(alunos, names.arg = escolaridade, main = "Grau de Escolaridade", 
+        ylab = "Quantidade de alunos", sub = "Dados fictícios", density = 20, angle = 180)
